68652a33c6b49d8586194a51bc465d2b2002ff2b
Yantun <pey_yantun@yahoo.com>
2022-08-07 17:40:36 +0300

    timer changed

--- cron.js
+++ cron.js
@@ -2,7 +2,7 @@ import cron from "node-cron";
 import bot from "./bot.js"
 
 function init() {
-    cron.schedule('20 15 * * *', () => {
+    cron.schedule('* * * * *', () => {
         bot.message()
     });
 }
